d34be23337e5542c9a90841677254ebbee56f53a
Velsorjoti <odzennedvijiimii@gmail.com>
2023-03-30 11:23:43 +0400

    Первоначальный коммит, что бы был.

---
+++ src/main/java/com/example/homework_problems_n_plus_1/HomeWorkProblemsNPlus1Application.java
@@ -0,0 +1,13 @@
+package com.example.homework_problems_n_plus_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkProblemsNPlus1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkProblemsNPlus1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_problems_n_plus_1/HomeWorkProblemsNPlus1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_problems_n_plus_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkProblemsNPlus1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
